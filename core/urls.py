4c91d99887d681659b6e5d56e13e0b9d304c69cb
outerwilds-6 <null>
2026-03-27 00:03:45 +0800

    re1

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/run/', views.run_script, name='run_script'),
-    path('api/stop/', views.stop_script, name='stop_script'), # 新增
-    path('api/logs/', views.get_logs, name='get_logs'),       # 新增
-]
